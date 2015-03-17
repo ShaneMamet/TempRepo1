5d39b7c1f1f68c4219836649aff8298a295b0c97
Shane Mamet <shane@legalsuite.co.za>
2015-03-17 08:37:52 +0200

    Removed StripData
    Removed StripData

--- database/StripData.php
+++ database/StripData.php
@@ -1,11 +1,6 @@
 <?php
 
 //http://css-tricks.com/serious-form-security/
-function StripData($s){
-		// Restores the added slashes (ie.: " I\'m John " for security in output, and escapes them in htmlentities(ie.:  &quot; etc.)
-		// Also strips any <html> tags it may encouter
-		// Use: Anything that shouldn't contain html (pretty much everything that is not a textarea)
-		return replaceSingleQuotes(htmlentities(trim(strip_tags(stripslashes($s))), ENT_NOQUOTES, "UTF-8"));
 }
 
 function StripContent($s){
